576bd4c961bb16503e07a3c2bfbe70835b86fa12
Darya_Kasperovich <dashkasperovich@icloud.com>
2026-09-08 01:21:16 +0300

    Add sys import to wsgi.py

--- paradise/wsgi.py
+++ paradise/wsgi.py
@@ -1,14 +1,8 @@
-"""
-WSGI config for paradise project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys  # ✅ ДОБАВЬ ЭТУ СТРОКУ
 
+# Добавляем путь к корневой папке
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from django.core.wsgi import get_wsgi_application
 
