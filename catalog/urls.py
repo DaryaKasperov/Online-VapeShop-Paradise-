94abe85d3d801ce1ab3322cc96df5431b8ef02dc
Darya_Kasperovich <dashkasperovich@icloud.com>
2026-09-09 18:49:15 +0300

    nova

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,9 +4,8 @@ from . import views
 app_name = 'catalog'
 
 urlpatterns = [
-path('product/<slug:slug>/review/', views.add_review, name='add_review'),
     path('', views.product_list, name='product_list'),
     path('category/<slug:category_slug>/', views.product_list, name='category'),
     path('product/<slug:slug>/', views.product_detail, name='product_detail'),
-    ]
-
+    path('product/<slug:slug>/review/', views.add_review, name='add_review'),
+]
